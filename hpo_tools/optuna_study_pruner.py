ab624c15b963d13172ed6fd14e8202a1358c255c
Philip May <philip@may.la>
2022-02-15 09:50:11 +0100

    add copyright for Sigrun May in optuna_study_pruner.py

--- hpo_tools/optuna_study_pruner.py
+++ hpo_tools/optuna_study_pruner.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2022 Sigrun May, Ostfalia Hochschule für angewandte Wissenschaften
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
 """Optuna study pruner."""
 
 import math
